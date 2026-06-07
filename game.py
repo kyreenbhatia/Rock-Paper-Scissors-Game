f7d601e1c545d164ce0f453832e7ee2123b20025
Kyreen Bhatia <kyreen22bhatia@gmail.com>
2026-06-07 19:10:49 +0530

    Implement Rock, Paper, Scissors game

---
+++ game.py
@@ -0,0 +1,27 @@
+import random 
+item_list = ["Rock", "Paper", "Scissor"]
+
+user_choice = input("Enter your move = Rock, Paper, Scissor = ")
+comp_choice = random.choice(item_list)
+
+print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
+if user_choice == comp_choice: 
+    print("both chooses same: = Match Tie")
+
+elif user_choice == "Rock": 
+    if comp_choice == "Paper": 
+        print("Paper covers Rock = Computer wins")
+    else: 
+        print("Rock smashes Scissors = User wins")
+
+elif user_choice == "Paper": 
+    if comp_choice == "Scissor": 
+        print("Scissor cuts Paper = Computer wins")
+    else: 
+        print("Paper covers Rock = User wins")
+
+elif user_choice == "Scissor": 
+    if comp_choice == "Paper": 
+        print("Scissor cuts Paper = You win")
+    else:
+        print("Rock smashes Scissor = Computer wins")
